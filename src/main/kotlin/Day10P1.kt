51d8b8a0efde48c0f89c6aff69d11078e35e428d
James Thomas <thejamesthomas@gmail.com>
2021-12-09 23:52:21 -0600

    Solve Day10P1

---
+++ src/main/kotlin/Day10P1.kt
@@ -0,0 +1,65 @@
+import java.util.*
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+fun main() {
+    val input = Path("src/main/resources/Day10Input.txt").readText().split("\n")
+    val solutionValue = Day10P1().solve(input)
+    println(solutionValue)
+}
+
+class Day10P1 {
+    fun solve(inputLines: List<String>): Int {
+        return inputLines.sumOf { line ->
+            val (_, corruptedChar) = line.isCorrupted()
+
+            corruptedChar.pointValue()
+        }
+    }
+}
+
+private fun Char.pointValue(): Int {
+    return when (this) {
+        ')' -> 3
+        ']' -> 57
+        '}' -> 1197
+        '>' -> 25137
+        else -> 0
+    }
+}
+
+fun String.isCorrupted(): Pair<Boolean, Char> {
+    val stack = Stack<Char>()
+
+    this.toCharArray().forEach { currentChar ->
+        if (currentChar.isOpeningCharacter()) {
+            stack.push(currentChar)
+        }
+        if (currentChar.isClosingCharacter()) {
+            if (stack.size > 0 && stack.peek().isMatchingOpeningCharacterFor(currentChar))
+                stack.pop()
+            else
+                return true to currentChar
+        }
+    }
+
+    return false to 'X'
+}
+
+private fun Char.isMatchingOpeningCharacterFor(closingCharacter: Char): Boolean {
+    return when (closingCharacter) {
+        ')' -> this == '('
+        ']' -> this == '['
+        '}' -> this == '{'
+        '>' -> this == '<'
+        else -> false
+    }
+}
+
+private fun Char.isClosingCharacter(): Boolean {
+    return this in listOf(')', ']', '}', '>')
+}
+
+private fun Char.isOpeningCharacter(): Boolean {
+    return this in listOf('(', '[', '{', '<')
+}
---
+++ src/test/kotlin/Day10P1Test.kt
@@ -0,0 +1,69 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day10P1Test {
+
+    @Test
+    fun isCorrupted_corruptedLineShouldBeMarkedAsCorrupted1() {
+        val corruptedLine = "{([(<{}[<>[]}>{[]{[(<()>"
+
+        val (actualStatus, actualIncorrectCharacter) = corruptedLine.isCorrupted()
+        assertThat(actualStatus).isEqualTo(true)
+        assertThat(actualIncorrectCharacter).isEqualTo('}')
+    }
+
+    @Test
+    fun isCorrupted_corruptedLineShouldBeMarkedAsCorrupted2() {
+        val corruptedLine = "[[<[([]))<([[{}[[()]]]"
+
+        val (actualStatus, actualIncorrectCharacter) = corruptedLine.isCorrupted()
+        assertThat(actualStatus).isEqualTo(true)
+        assertThat(actualIncorrectCharacter).isEqualTo(')')
+    }
+
+    @Test
+    fun isCorrupted_corruptedLineShouldBeMarkedAsCorrupted3() {
+        val corruptedLine = "[{[{({}]{}}([{[{{{}}([]"
+
+        val (actualStatus, actualIncorrectCharacter) = corruptedLine.isCorrupted()
+        assertThat(actualStatus).isEqualTo(true)
+        assertThat(actualIncorrectCharacter).isEqualTo(']')
+    }
+    @Test
+    fun isCorrupted_corruptedLineShouldBeMarkedAsCorrupted4() {
+        val corruptedLine = "[<(<(<(<{}))><([]([]()"
+
+        val (actualStatus, actualIncorrectCharacter) = corruptedLine.isCorrupted()
+        assertThat(actualStatus).isEqualTo(true)
+        assertThat(actualIncorrectCharacter).isEqualTo(')')
+    }
+    @Test
+    fun isCorrupted_corruptedLineShouldBeMarkedAsCorrupted5() {
+        val corruptedLine = "<{([([[(<>()){}]>(<<{{"
+
+        val (actualStatus, actualIncorrectCharacter) = corruptedLine.isCorrupted()
+        assertThat(actualStatus).isEqualTo(true)
+        assertThat(actualIncorrectCharacter).isEqualTo('>')
+    }
+
+    @Test
+    fun isCorrupted_uncorruptedLineShouldNotBeMarkedAsCorrupted1() {
+        val corruptedLine = "[({(<(())[]>[[{[]{<()<>>"
+
+        val (actualStatus, actualIncorrectCharacter) = corruptedLine.isCorrupted()
+        assertThat(actualStatus).isEqualTo(false)
+        assertThat(actualIncorrectCharacter).isEqualTo('X')
+    }
+
+    @Test
+    fun isCorrupted_uncorruptedLineShouldNotBeMarkedAsCorrupted2() {
+        val corruptedLine = "[(()[<>])]({[<{<<[]>>("
+
+        val (actualStatus, actualIncorrectCharacter) = corruptedLine.isCorrupted()
+        assertThat(actualStatus).isEqualTo(false)
+        assertThat(actualIncorrectCharacter).isEqualTo('X')
+    }
+}
+
